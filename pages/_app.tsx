f95778529f971c86faf5b13a6da48909da36ec7a
Maksim Karalevich <m@karalevich.com>
2022-09-08 15:26:04 -0700

    refactor: update gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import type { AppProps } from "next/app";
-import TagManager from "react-gtm-module";
 import { globalStyles } from "../styles/global";
 
 function App({ Component, pageProps }: AppProps) {
 	globalStyles();
-
-	// Google Tag Manager
-	React.useEffect(() => {
-		TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM as string });
-	}, []);
 
 	return (
 		<>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Subscribe from "../components/subscribe";
 import { Flex } from "../components/primitives";
 import Head from "next/head";
 import { motion } from "framer-motion";
+import Script from "next/script";
 
 const title = "GOJI · Education tracker";
 const ogTitle = "GOJI · Organize education for your kids";
@@ -31,6 +32,24 @@ export default function Index() {
 				<meta property="image:height" content="720" />
 				<title>{title}</title>
 			</Head>
+
+			<Script
+				async
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM}`}
+			/>
+			<Script
+				id="gtm"
+				dangerouslySetInnerHTML={{
+					__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GTM}', {
+              page_path: window.location.pathname,
+            });
+          `,
+				}}
+			/>
 
 			<Flex
 				as={motion.main}
